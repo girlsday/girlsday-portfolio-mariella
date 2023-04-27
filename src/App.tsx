54fe5430bef1ef3442f87356cd70d2b6c5867d23
girlsday <girlsday@bitgrip.de>
2023-04-27 15:32:45 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button, Card, Container, MantineProvider, Text, Title, Image } from "@mantine/core";
+import { Button, Card, Container, MantineProvider, Text, Title, Image, Center, Group, Stack, Grid, Flex } from "@mantine/core";
 import { Carousel } from '@mantine/carousel';
 
 function App() {
@@ -10,36 +10,61 @@ function App() {
 
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container>
+
+   
+        <Center maw={400} h={100} mx="auto">
+          <Text size={100}>Mariella </Text>
+        </Center>
+
+        <Flex 
+          direction={{ base: 'column', sm: 'row' }}
+          gap={{ base: 'sm', sm: 'lg' }}
+          justify={{sm: 'center' }}
+          align={"center"}>
+            
+          <Image m={"md"} maw={260} src="/girlsday-portfolio-mariella/GirlsDay-2.jpg" alt="Profil Image" />
+          <Group>
+          <Text m={"md"}>
+                  Ich bin Mariella, im Moment 12 und ich habe zwei Katzen. Ich gehe in die siebte Klasse,
+                  habe einen kleinen Brunder und weiß jetzt schon nicht mehr was ich schreiben soll.
+                  Meine Hobbys zur Zeit sind Hip Hop tanzen, eigentlich generell Sport machen, zeichnen und Musik hören. 
+          </Text>
+            <Center >
+              <Button m={"md"} color="red" radius="xs" size="md" onClick={()=> {alert("COOL das DU mich gedrückt hast!!!!!")}}>DRÜCK MICH</Button>
+            </Center>
+          </Group>
+  
+          
+                </Flex>
+                
+        
 
 
+      <Grid justify="center" my={60}>
+        <Grid.Col span={4}>
+          <Image mt={8} miw={200} src={"/girlsday-portfolio-mariella/GirlsDay-4.jpg"}></Image>
+        </Grid.Col>
+        <Grid.Col span={2}>
+        <Image src={"/girlsday-portfolio-mariella/GirlsDay-7.jpg"}></Image>
+      
+        </Grid.Col>
+      </Grid>
 
-    <Image maw={240} mx="auto" radius="md" src="/girlsday-portfolio-mariella/GirlsDay-2.jpg" alt="Profil Image" />
-    <Text size={100}>Mariella </Text>
-    <Carousel slideSize="70%" height={200} slideGap="xl" controlsOffset="md" controlSize={30} loop draggable={false}>
+    <Carousel slideSize="70%"  slideGap="xl" controlsOffset={500} controlSize={30} loop draggable={false}>
       {/* ...slides */}
       <Carousel.Slide> <Image maw={240} mx="auto" radius="md" src="/girlsday-portfolio-mariella/GirlsDay-3.jpg" alt="Profil Image" /></Carousel.Slide>
-      <Carousel.Slide> <Text size={50}>wir...</Text></Carousel.Slide>
-      <Carousel.Slide> <Text size={50}>beobachten...</Text></Carousel.Slide>
-      <Carousel.Slide> <Text size={50}>DICH!!!</Text></Carousel.Slide>
+      <Carousel.Slide> <Image fit="scale-down" height={"350"} radius="md" src="/girlsday-portfolio-mariella/IMG_0347.jpg" alt="Profil Image" /></Carousel.Slide>
+      <Carousel.Slide> <Image maw={240} mx="auto" radius="md" src="/girlsday-portfolio-mariella/GirlsDay-5.jpg" alt="Profil Image" /></Carousel.Slide>
+      <Carousel.Slide> <Image fit="scale-down" height={"350"} radius="md" src="/girlsday-portfolio-mariella/IMG_0460.jpg" alt="Profil Image" /></Carousel.Slide>
     </Carousel>
-
-
-
         <Card mt="xl">
           
           <Text mt="sm">
             
           </Text>
         </Card>
-        <Button color="red" radius="xs" size="md">
-      DRÜCK MICH 
-    </Button>
-        <Text mt="md" c="dimmed">
-          Meine Hobbys...
-          Hobby los sein :) 
-        </Text>
-      </Container>
+
+ 
     </MantineProvider>
   );
 }
